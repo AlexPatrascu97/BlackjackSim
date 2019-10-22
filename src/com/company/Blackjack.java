e08ff0fd90aa3aa116ef327617393c7cb8227208
Alexandru Patrascu Mihail <frostyalex97@gmail.com>
2019-10-22 19:44:08 +0300

    hidden card

--- src/com/company/Blackjack.java
+++ src/com/company/Blackjack.java
@@ -1,27 +1,45 @@
 package com.company;
 
-
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Blackjack
 {
     public static void main(String[] args)
     {
-        // number of games
-        int playCount = 100;
-
         int dealerwins = 0;
         int playerwins = 0;
+        int drawresult = 0;
 
         Game player = new Game();
         Game dealer = new Game();
 
+        Scanner in = new Scanner(System.in);
+        System.out.println("Legend for dealer: \n" +
+            "low risk means he wil stop after reaching a number greater than 8\n"+
+            "low-med riskmeans he wil stop after reaching a number greater than 10\n"+
+            "med risk means he wil stop after reaching a number greater than 12\n"+
+            "med-high risk means he wil stop after reaching a number greater than 14\n"+
+            "high risk means he wil stop after reaching a number greater than 16\n");
+        System.out.println("Legend for Player: \n" +
+                "low risk means he wil stop after reaching a number greater than 12\n"+
+                "low-med riskmeans he wil stop after reaching a number greater than 14\n"+
+                "med risk means he wil stop after reaching a number greater than 16\n"+
+                "med-high risk means he wil stop after reaching a number greater than 18\n"+
+                "high risk means he wil aim for 21\n");
 
-        for (int i=0; i<=playCount; i++) {
+        System.out.println("Insert the number of deals");
+        String counts = in.nextLine();
+        System.out.println("Insert Dealer risk (low risk, low-med risk, med risk, med-high risk, high risk)");
+        String riskdealer = in.nextLine();
+        System.out.println("Insert Player risk (low risk, low-med risk, med risk, med-high risk, high risk)");
+        String riskpayer = in.nextLine();
 
-            dealer.dealer();
-            player.player();
+
+        for (int i=0; i <= Integer.parseInt(counts) - 1 ; i++) {
+
+            //initializare player si dealer
+            dealer.dealer(riskdealer);
+            player.player(riskpayer);
 
 
             //verificare blackjack pentru dealer si player
@@ -58,12 +76,22 @@ public class Blackjack
 
             } else if ((player.result == 21 && dealer.result == 21) || (player.result > 21 && dealer.result > 21)) {
                 //situatia de blackjack egal sau ambele valori peste 21
+                drawresult++;
                 System.out.println("No winner, Next Deal");
             }
 
         }
 
+        float playerpercent = ((float)(playerwins)/(float)(Integer.parseInt(counts)))*100;
+        float dealerpercent = ((float)(dealerwins)/(float)(Integer.parseInt(counts)))*100;
+        float kda = ((float)(playerwins)/(float)(dealerwins));
+
         System.out.println("Player Total wins: " + playerwins);
         System.out.println("Dealer Total wins: " + dealerwins);
+        System.out.println("No winner results: " + drawresult);
+
+        System.out.println("Player's win percent in " + riskpayer + " versus " + riskdealer +" is : " + playerpercent + "%");
+        System.out.println("Dealers's win percent in " + riskdealer + " versus " + riskpayer +" is : " + dealerpercent + "%" );
+        System.out.println("Players win/lose ratio in " + riskpayer + " versus " + riskdealer +" is : " + kda);
     }
 }
--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,8 +1,5 @@
 package com.company;
 
-import com.company.Card;
-import com.company.Deck;
-
 import java.util.ArrayList;
 
 public class Game
@@ -16,7 +13,7 @@ public class Game
 
 
     //strategie dealer
-    public int dealer() {
+    public int dealer(String strat) {
 
         acesInHand = 0;
         deck.shuffle();
@@ -24,44 +21,108 @@ public class Game
         result = deck.getCard(cardNumber).getCount();
         hand = new ArrayList<>();
 
-        //high risk 21
-        while (result < 21) {
-            cardNumber++;
-            if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+        switch(strat) {
 
-                acesInHand++;
+            //high risk 16
+            case "high risk":
+            while (result < 16) {
+                cardNumber++;
+                if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                    acesInHand++;
+                }
+                hand.add(deck.getCards().get(cardNumber));
+                System.out.println(deck.getCards().get(cardNumber).getName());
+                result = calculateResult(hand);
             }
-            hand.add(deck.getCards().get(cardNumber));
-            System.out.println(deck.getCards().get(cardNumber).getName());
-            result = calculateResult(hand);
+            //hidden card
+                cardNumber++;
+                hand.add(deck.getCards().get(cardNumber));
+                System.out.println("Hidden card:" + deck.getCards().get(cardNumber).getName());
+                result = calculateResult(hand);
+
+            System.out.println("Rezultat: " + result + "\n");
+
+                //medium-high risk 14
+            case "med-high risk":
+                while (result < 14) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                //hidden card
+                cardNumber++;
+                hand.add(deck.getCards().get(cardNumber));
+                System.out.println("Hidden card:" + deck.getCards().get(cardNumber).getName());
+                result = calculateResult(hand);
+                System.out.println("Rezultat: " + result + "\n");
+
+                //medium risk 12
+            case "med risk":
+                while (result < 12) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                //hidden card
+                cardNumber++;
+                hand.add(deck.getCards().get(cardNumber));
+                System.out.println("Hidden card:" + deck.getCards().get(cardNumber).getName());
+                result = calculateResult(hand);
+                System.out.println("Rezultat: " + result + "\n");
+
+                //low-medium risk 10
+            case "low-med risk":
+                while (result < 10) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                //hidden card
+                cardNumber++;
+                hand.add(deck.getCards().get(cardNumber));
+                System.out.println("Hidden card:" + deck.getCards().get(cardNumber).getName());
+                result = calculateResult(hand);
+                System.out.println("Rezultat: " + result + "\n");
+
+
+                //low risk 8
+            case "low risk":
+                while (result < 8) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                //hidden card
+                cardNumber++;
+                hand.add(deck.getCards().get(cardNumber));
+                System.out.println("Hidden card:" + deck.getCards().get(cardNumber).getName());
+                result = calculateResult(hand);
+                System.out.println("Rezultat: " + result + "\n");
+
+
         }
-        System.out.println("Rezultat: " + result +"\n");
 
-        //medium risk 18
-        while (result < 18) {
-            cardNumber++;
-            if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
-
-                acesInHand++;
-            }
-            hand.add(deck.getCards().get(cardNumber));
-            System.out.println(deck.getCards().get(cardNumber).getName());
-            result = calculateResult(hand);
-        }
-        System.out.println("Rezultat: " + result +"\n");
-
-        //medium risk 18
-        while (result < 18) {
-            cardNumber++;
-            if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
-
-                acesInHand++;
-            }
-            hand.add(deck.getCards().get(cardNumber));
-            System.out.println(deck.getCards().get(cardNumber).getName());
-            result = calculateResult(hand);
-        }
-        System.out.println("Rezultat: " + result +"\n");
         if (result == 21) {
 
             System.out.println("BLACKJACK");
@@ -69,9 +130,8 @@ public class Game
         return result;
     }
 
-
     //strategie player
-    public int player() {
+    public int player(String strat) {
 
         acesInHand = 0;
         deck.shuffle();
@@ -79,31 +139,81 @@ public class Game
         result = deck.getCard(cardNumber).getCount();
         hand = new ArrayList<>();
 
-        //high risk
-        while (result < 21) {
-            cardNumber++;
-            if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+        switch(strat) {
 
-                acesInHand++;
-            }
-            hand.add(deck.getCards().get(cardNumber));
-            System.out.println(deck.getCards().get(cardNumber).getName());
-            result = calculateResult(hand);
+            //high risk 21
+            case "high risk":
+                while (result < 21) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                System.out.println("Rezultat: " + result + "\n");
+
+                //medium-high risk 18
+            case "med-high risk":
+                while (result < 18) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                System.out.println("Rezultat: " + result + "\n");
+
+                //medium risk 16
+            case "med risk":
+                while (result < 16) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                System.out.println("Rezultat: " + result + "\n");
+
+                //low-medium risk 12
+            case "low-med risk":
+                while (result < 14) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                System.out.println("Rezultat: " + result + "\n");
+
+
+                //low risk 10
+            case "low risk":
+                while (result < 12) {
+                    cardNumber++;
+                    if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
+
+                        acesInHand++;
+                    }
+                    hand.add(deck.getCards().get(cardNumber));
+                    System.out.println(deck.getCards().get(cardNumber).getName());
+                    result = calculateResult(hand);
+                }
+                System.out.println("Rezultat: " + result + "\n");
+
+
         }
-        System.out.println("Rezultat: " + result +"\n");
-
-        //low risk 18
-        while (result < 18) {
-            cardNumber++;
-            if (deck.getCards().get(cardNumber).getSecondCount() > 0) {
-
-                acesInHand++;
-            }
-            hand.add(deck.getCards().get(cardNumber));
-            System.out.println(deck.getCards().get(cardNumber).getName());
-            result = calculateResult(hand);
-        }
-        System.out.println("Rezultat: " + result +"\n");
 
         if (result == 21) {
 
